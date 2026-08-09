06527ae98ba7269997befd07d2d7f4572fd70597
grifisu <zvagincevkiril@gmail.com>
2026-08-09 17:34:28 +0300

    Add files via upload

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
-    path('tasks/', include('tasks.urls')),
+    path('tasks/', include('tasks.urls', namespace='tasks')),
 ]
